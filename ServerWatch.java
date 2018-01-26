56ca7e759799284bcf482812d5d5857f2f509d33
ButterBreeze <thbgyujnbvfredc1A>
2018-01-26 12:19:24 -0600

    Bases for the backup event handler

--- ServerWatch.java
+++ ServerWatch.java
@@ -1,6 +1,13 @@
-import java.io.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Calendar;
+
 
 public class ServerWatch {
 
@@ -9,6 +16,7 @@ public class ServerWatch {
             ServerSocket instructionServerSocket = new ServerSocket(2048);
             instructionServerSocket.accept();
             Socket instructionSocket = null;
+
 
             while (true) {
 
@@ -30,6 +38,7 @@ public class ServerWatch {
      *               and kill the socket
      */
     public static void socketConnection(Socket socket) {
+
         Thread instructionThread = new Thread() {
             public void run() {
                 instructions(socket);
@@ -39,25 +48,84 @@ public class ServerWatch {
         instructionThread.start();
     }
 
+
+    /**
+     * this will see what the connection is asking the server to do
+     *
+     * @param socket ~ the socket the computer connects
+     *               TODO: connect using username and password
+     */
     public static void instructions(Socket socket) {
         PrintWriter output = null;
         BufferedReader input = null;
 
-        try{
-            input = new BufferedReader( new InputStreamReader(socket.getInputStream()));
+        try {
+            input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             output = new PrintWriter(socket.getOutputStream(), true);
 
 
+            output.write("ready");
+            String instructions = input.readLine();
 
+            if (instructions == "TabletBackup") { //Backup the tablet files/oasis
 
-        output.write("ready");
-        String instructions = input.readLine();
+            }
 
-        }catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
 
     }
 
+    public static void startBackupCountdown() {
+
+
+        Thread backupThread = new Thread() {
+            public void run() {
+                initializeBackup();
+            }
+        };
+        backupThread.start();
+    }
+
+    public static void initializeBackup() {
+
+
+        int untilBackupMills = getMillsToMidnight();
+        ActionListener actionListener = new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                doBackup();
+            }
+        };
+
+        Timer timer = new Timer(untilBackupMills, actionListener);
+        timer.start();
+
+    }//end method
+
+    public static void doBackup() {
+
+
+    }
+
+    /**
+     *
+     * @return time till midnight, returns int because int will be able to handle the amount we need
+     *              (upto 24 days which is past midnight from anytime)
+     */
+    public static int getMillsToMidnight(){
+
+
+        Calendar c = Calendar.getInstance();
+        c.add(Calendar.DAY_OF_MONTH, 1);
+        c.set(Calendar.HOUR_OF_DAY, 0);
+        c.set(Calendar.MINUTE, 0);
+        c.set(Calendar.SECOND, 0);
+        c.set(Calendar.MILLISECOND, 0);
+        return  (int) (c.getTimeInMillis()-System.currentTimeMillis());
+
+    }
 
 }//end class
